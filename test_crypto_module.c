d501de827c2cc7558db7e20268e030e98b711fbe
Dmitry Falko <dfalko@digiflak.com>
2014-04-08 11:42:33 +0400

    Refactoring

--- test_crypto_module.c
+++ test_crypto_module.c
@@ -26,20 +26,16 @@ static int crypt_data(u8 *key, u32 key_len, u8 *iv, u32 iv_len, u8 *dst, u32 dst
 
     /*CRYPTO_ALG_TYPE_BLKCIPHER_MASK, CRYPTO_ALG_TYPE_BLKCIPHER*/
 	blk = crypto_alloc_blkcipher("xts(aes)", 0, 0);
-	if ( IS_ERR(blk) )
-	{
+	if (IS_ERR(blk)) {
 		printk(KERN_ALERT "Failed to initialize AES-XTS mode \n");
 		return -1;
-	}
-	else 
-	{
+	} else {
 		printk(KERN_ALERT "Initialized cipher: %s \n", crypto_blkcipher_name(blk));
 		printk(KERN_ALERT "with IV size: %d \n", crypto_blkcipher_ivsize(blk));
 		printk(KERN_ALERT "block size: %d \n", crypto_blkcipher_blocksize(blk));
 	}
 
-	if(crypto_blkcipher_setkey(blk, key, key_len))
-	{
+	if(crypto_blkcipher_setkey(blk, key, key_len)) {
 		printk(KERN_ALERT "Failed to set key. \n");
 		goto err;
 	}
@@ -53,17 +49,12 @@ static int crypt_data(u8 *key, u32 key_len, u8 *iv, u32 iv_len, u8 *dst, u32 dst
 	desc.tfm = blk;
 	desc.flags = 0;
 
-	if(mode == ENCRYPT)
-	{
-		if(crypto_blkcipher_encrypt(&desc, &sg[1], &sg[0], src_len))
-		{
+	if(mode == ENCRYPT) {
+		if(crypto_blkcipher_encrypt(&desc, &sg[1], &sg[0], src_len)) {
 			printk(KERN_ALERT "Failed to encrypt. \n");
 		}
-	}
-	else
-	{
-		if(crypto_blkcipher_decrypt(&desc, &sg[1], &sg[0], src_len))
-		{
+	} else {
+		if(crypto_blkcipher_decrypt(&desc, &sg[1], &sg[0], src_len)) {
 			printk(KERN_ALERT "Failed to encrypt. \n");
 		}
 	}
@@ -97,13 +88,9 @@ static int aes_crypt_init(void)
 	err = crypt_data(key, AES_KEY_SIZE, iv, AES_IV_SIZE, dec, AES_BLOCK_SIZE, enc, AES_BLOCK_SIZE, DECRYPT);
 
 	if(memcmp(dec, src, AES_BLOCK_SIZE))
-	{
 		printk(KERN_ALERT "Encrypt/Decrypt error. \n");
-	}
 	else
-	{
 		printk(KERN_ALERT "Test passed. \n");
-	}
 
 	return err;
 }
